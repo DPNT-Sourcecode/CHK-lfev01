183c1e6c166b5a552c2713ca13dca01157546592
sbx_user1051 <sbx_user1051@169.254.2.121>
2025-04-02 10:09:51 +0000

    Wed Apr 02 10:09:51 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,12 +5,26 @@ def checkout(skus):
     total = 0 
     tally = {'A': 0, 'B':0, 'C': 0, 'D': 0}
 
-    for char in 
-    
+    for char in skus:
+        if char in tally:
+            tally[char] += 1
+
+
+
+    tally += (tally['A'] % 3) * 50
+    tally += (tally['A'] // 3) * 130
+    tally += (tally['A'] % 3) * 50
+    tally += (tally['A'] // 3) * 130
+    total += tally['C'] * 20
+    total += tally['D'] * 15
+
+
+
     
         
 
     
+
 
 
 
