156b0f65b16f7e7c92902e926412ed385781456a
sbx_user1051 <sbx_user1051@169.254.2.121>
2025-04-02 10:03:51 +0000

    Wed Apr 02 10:03:51 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,7 +1,13 @@
-
+from collections import Counter
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    for x in skus:
-        
+    tally = [0, 0, 0, 0]
+    counter = Counter(skus)
+
+    tally = [counter.get(chr(i), 0) for i in range(ord)]
+
+    
+
+
